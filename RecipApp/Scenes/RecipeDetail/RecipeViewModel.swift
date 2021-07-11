a40672e09b2a95792ea248336b8edbf34ae80ec5
DanyST <lherreralillo@gmail.com>
2021-07-10 23:09:38 -0400

    Add RecipeDetailViewModel and RecipeDetailViewModelCoordinatorDelegate, RecipeDetailViewModelViewDelegate, RecipeDetailViewModelProtocol protocols

---
+++ RecipApp/Scenes/RecipeDetail/RecipeViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  RecipeViewModel.swift
+//  RecipApp
+//
+//  Created by Luis Herrera Lillo on 10-07-21.
+//
+
+import Foundation
+
+
+protocol RecipeDetailViewModelCoordinatorDelegate: AnyObject {}
+
+protocol RecipeDetailViewModelViewDelegate: AnyObject {}
+
+protocol RecipeDetailViewModelProtocol {
+    var numberOfSections: Int { get }
+    func numberOfRows(in section: Int) -> Int
+}
+
+class RecipeDetailViewModel: RecipeListViewModelProtocol {
+    
+    // MARK: - Properties
+    weak var coordinatorDelegate: RecipeDetailViewModelCoordinatorDelegate?
+    weak var viewDelegate: RecipeDetailViewModelViewDelegate?
+    
+    var numberOfSections: Int {
+        return 1
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        return 1
+    }
+}
