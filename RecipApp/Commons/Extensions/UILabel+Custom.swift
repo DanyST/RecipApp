4a5d668e43d6345b4be8d9ebc8548c70aeb79490
DanyST <lherreralillo@gmail.com>
2021-07-12 01:34:00 -0400

    Add UIlabel+Custom extension

---
+++ RecipApp/Commons/Extensions/UILabel+Custom.swift
@@ -0,0 +1,33 @@
+//
+//  UILabel+Custom.swift
+//  RecipApp
+//
+//  Created by Luis Herrera Lillo on 12-07-21.
+//
+
+import UIKit
+
+extension UILabel {
+    static var customTitleLabel: UILabel {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
+    
+    static var customSubTitleLabel: UILabel = {
+        let label = UILabel.customTitleLabel
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        return label
+    }()
+    
+    static var customSecondaryLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+}
